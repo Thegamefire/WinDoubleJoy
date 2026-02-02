498598015f6f3ab903abd24e10295d9bf3aee383
Game Fire <code@thegamefire.com>
2026-02-02 17:21:38 +0100

    Added Connecting And Subscribing to Characteristics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use btleplug::api::{bleuuid::BleUuid, Central, CentralEvent, Manager as _, ScanFilter};
+use btleplug::api::{bleuuid::BleUuid, Central, CentralEvent, Manager as _, Peripheral, ScanFilter};
 use btleplug::platform::{Adapter, Manager};
 use futures::stream::StreamExt;
+use uuid::{uuid, Uuid};
 
 async fn get_central(manager: &Manager) -> Adapter {
     let adapters = manager.adapters().await.unwrap();
     adapters.into_iter().nth(0).unwrap()
 }
-
 
 #[tokio::main]
 async fn main() {
@@ -26,7 +26,18 @@ async fn main() {
             } => {
                 if (manufacturer_data.contains_key(&0x0553)
                     && manufacturer_data[&0x0553]==[1, 0, 3, 126, 5, 103, 32, 0, 1, 0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0]) {
-                    println!("{:?}", manufacturer_data);
+                    central.peripheral(&id).await.unwrap().connect().await.unwrap();
+                }
+            }
+            CentralEvent::DeviceConnected(id) => {
+                for characteristic in  central.peripheral(&id).await.unwrap().characteristics() {
+                    if (characteristic.service_uuid==uuid!("cc1bbbb5-7354-4d32-a716-a81cb241a32a")) {
+                        // JoyConLeft
+                        central.peripheral(&id).await.unwrap().subscribe(&characteristic).await.unwrap();
+                    } else if (characteristic.service_uuid==uuid!("d5a9e01e-2ffc-4cca-b20c-8b67142bf442")) {
+                        // JoyConRight
+                        central.peripheral(&id).await.unwrap().subscribe(&characteristic).await.unwrap();
+                    }
                 }
             }
             _ => {}
