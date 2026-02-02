bf9ce4386b2e6c9266479190dd93edfa33adb922
Hannes <hannes-dev@mailbox.org>
2026-02-02 23:22:28 +0100

    cleanup a bit

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use btleplug::api::{
 };
 use btleplug::platform::{Adapter, Manager, Peripheral};
 use futures::stream::StreamExt;
-use uuid::uuid;
+use uuid::{Uuid, uuid};
 
 pub mod bluetooth;
+
+const JOYCONLEFT_UUID: Uuid = uuid!("cc1bbbb5-7354-4d32-a716-a81cb241a32a");
+const JOYCONRIGHT_UUID: Uuid = uuid!("d5a9e01e-2ffc-4cca-b20c-8b67142bf442");
 
 async fn get_central(manager: &Manager) -> Adapter {
     let adapters = manager.adapters().await.unwrap();
@@ -37,9 +40,9 @@ async fn main() {
                 id,
                 manufacturer_data,
             } => {
-                if manufacturer_data.contains_key(&0x0553)
-                    && manufacturer_data[&0x0553]
-                        == [
+                if let Some(data) = manufacturer_data.get(&0x0553)
+                    && data
+                        == &[
                             1, 0, 3, 126, 5, 103, 32, 0, 1, 0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0,
                             0, 0,
                         ]
@@ -63,26 +66,31 @@ async fn main() {
             CentralEvent::DeviceConnected(id) => {
                 let peripheral = central.peripheral(&id).await.unwrap();
                 for characteristic in peripheral.characteristics() {
-                    if characteristic.service_uuid == uuid!("cc1bbbb5-7354-4d32-a716-a81cb241a32a")
-                    {
-                        // JoyConLeft
-                        peripheral.subscribe(&characteristic).await.unwrap();
-                        peripheral
-                            .write(
-                                &get_command_characteristic(&peripheral).unwrap(),
-                                &[
-                                    0x09, 0x91, 0x00, 0x07, 0x00, 0x08, 0x00, 0x00, 0b1001, 0x00,
-                                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                ],
-                                WriteType::WithoutResponse,
-                            )
-                            .await
-                            .unwrap();
-                    } else if characteristic.service_uuid
-                        == uuid!("d5a9e01e-2ffc-4cca-b20c-8b67142bf442")
-                    {
-                        // JoyConRight
-                        peripheral.subscribe(&characteristic).await.unwrap();
+                    match characteristic.service_uuid {
+                        JOYCONLEFT_UUID => {
+                            // JoyConLeft
+                            peripheral.subscribe(&characteristic).await.unwrap();
+                            peripheral
+                                .write(
+                                    &get_command_characteristic(&peripheral).unwrap(),
+                                    &[
+                                        0x09, 0x91, 0x00, 0x07, 0x00, 0x08, 0x00, 0x00, 0b1001,
+                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                    ],
+                                    WriteType::WithoutResponse,
+                                )
+                                .await
+                                .unwrap();
+                        }
+                        JOYCONRIGHT_UUID => {
+                            // JoyConRight
+                            peripheral.subscribe(&characteristic).await.unwrap();
+                            let mut stream = peripheral.notifications().await.unwrap();
+                            while let Some(msg) = stream.next().await {
+                                dbg!(msg);
+                            }
+                        }
+                        _ => {}
                     }
                 }
             }
