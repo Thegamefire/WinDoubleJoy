ccf6534e13552bd1b6503af9699fd272def79e51
Game Fire <code@thegamefire.com>
2026-02-03 19:15:23 +0100

    Implemented apply_to(Gamepad) For Controller States

--- src/bluetooth/state.rs
+++ src/bluetooth/state.rs
@@ -1,4 +1,5 @@
 use btleplug::api::ValueNotification;
+use vigem_client::{XButtons, XGamepad};
 
 #[derive(Debug)]
 pub enum ControllerState {
@@ -40,6 +41,49 @@ pub struct RightJoyConState {
     stick_x: f32,
     stick_y: f32,
 }
+
+impl LeftJoyConState {
+    pub fn apply_to(&self, gamepad: &mut XGamepad) {
+        for (button, output) in &[
+            (self.up, XButtons!(UP)),
+            (self.down, XButtons!(DOWN)),
+            (self.left, XButtons!(LEFT)),
+            (self.right, XButtons!(RIGHT)),
+            (self.minus, XButtons!(BACK)),
+            (self.l, XButtons!(LB)),
+            (self.stick, XButtons!(LTHUMB)),
+        ] {
+            if *button { gamepad.buttons.raw |= output.raw; }
+        }
+        if self.zl {
+            gamepad.left_trigger = u8::MAX;
+        }
+        gamepad.thumb_lx = (self.stick_x * (i16::MAX as f32)).round() as i16;
+        gamepad.thumb_ly = (self.stick_y * (i16::MAX as f32)).round() as i16;
+    }
+}
+impl RightJoyConState {
+    pub fn apply_to(&self, gamepad: &mut XGamepad) {
+        for (button, output) in &[
+            (self.a, XButtons!(B)),
+            (self.b, XButtons!(A)),
+            (self.x, XButtons!(Y)),
+            (self.y, XButtons!(X)),
+            (self.plus, XButtons!(START)),
+            (self.r, XButtons!(RB)),
+            (self.home, XButtons!(GUIDE)),
+            (self.stick, XButtons!(RTHUMB)),
+        ] {
+            if *button { gamepad.buttons.raw |= output.raw; }
+        }
+        if self.zr {
+            gamepad.right_trigger = u8::MAX;
+        }
+        gamepad.thumb_rx = (self.stick_x * (i16::MAX as f32)).round() as i16;
+        gamepad.thumb_ry = (self.stick_y * (i16::MAX as f32)).round() as i16;
+    }
+}
+
 
 impl From<ValueNotification> for LeftJoyConState {
     fn from(notification: ValueNotification) -> Self {
