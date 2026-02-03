f359280cbae901f9bbbcf027406fcc0db462b2a3
Hannes <hannes-dev@mailbox.org>
2026-02-03 19:10:51 +0100

    non blocking recv

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,13 @@ async fn main() {
     let controller1 = manager.connect_controller().await.unwrap();
     dbg!(&controller1);
     if let Some(mut connection) = controller1 {
-        let msg = connection.update_receiver.blocking_recv();
+        let msg = connection.update_receiver.recv().await;
         dbg!(msg);
     }
     let controller2 = manager.connect_controller().await.unwrap();
     dbg!(&controller2);
     if let Some(mut connection) = controller2 {
-        let msg = connection.update_receiver.blocking_recv();
+        let msg = connection.update_receiver.recv().await;
         dbg!(msg);
     }
 }
